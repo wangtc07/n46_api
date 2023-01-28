e3c320a2abdf80fbf19dec7dc4f9d30a9968e13c
Wangtc07 <wangtc07@gmail.com>
2023-01-28 14:37:18 +0900

    create route

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 # save this as app.py
 import flask
-from routing import param
+from route.routing import param
+
 app = flask.Flask(__name__)
 app.register_blueprint(param)
+
+# JSON_AS_ASCII 初期值為 True, 中日文會亂碼
+app.config['JSON_AS_ASCII'] = False
+
+
+@app.after_request
+def after_request(res):
+    res.headers.add('Access-Control-Allow-Origin', '*')
+    res.headers.add('Access-Control-Allow-Headers', 'Content-Type')  # ORIGIN 跨域
+    res.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+
+    return res
+
 
 @app.route("/")
 @app.route("/hello")
 def hello():
     return "Hello, World!"
+
 
 if __name__ == '__main__':
     app.run()
--- routing.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-param = Blueprint('param',__name__)
-
-@param.route('/data/appInfo/<name>', methods=['GET'])
-def queryDataMessageByName(name):
-    print("type(name) : ", type(name))
-    return 'String => {}'.format(name)
