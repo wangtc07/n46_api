2219872ec4af024dd1c498b1e33471a4af333ade
Wangtc07 <wangtc07@gmail.com>
2023-01-29 13:36:52 +0900

    blog photo download api

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 # save this as app.py
 import flask
+from flask import current_app
 from route.routing import param
+from config import conifg
 
 app = flask.Flask(__name__)
 app.register_blueprint(param)
 
 # JSON_AS_ASCII 初期值為 True, 中日文會亂碼
 app.config['JSON_AS_ASCII'] = False
+
+# 配置 config class
+app.config.from_object(conifg.BaseConfig())
 
 
 @app.after_request
@@ -26,3 +31,7 @@ def hello():
 
 if __name__ == '__main__':
     app.run()
+    # base_path = current_app.config.get('BLOG_PHOTO_PATH')
+
+# def config():
+#     return app.config
--- route/routing.py
+++ route/routing.py
@@ -1,12 +1,13 @@
 import os
+import re
+import time
+import zipfile
 from io import BytesIO
-from typing import io
 
 import requests
 from flask import Blueprint, request, make_response, send_file, Response
 
-from service import blog_single
-from utils import down_utils
+from service import blog_single, blog_single_photo
 
 param = Blueprint('param', __name__)
 
@@ -32,3 +33,69 @@ def get_route():
     print(url)
     return url
 
+
+# 圖片直接顯示
+@param.route('/img', methods=['GET'])
+def down_img():
+    img_data = open(os.path.join('./', '矢久保美緒 blog 200427-1.jpg'), 'rb').read()
+    print(type(img_data))  # bytes
+    response = make_response(img_data)
+    response.headers['Content-Type'] = 'image/png'
+    return response
+
+
+# 直接下載
+@param.route('/img2', methods=['GET'])
+def down_img2():
+    return send_file('./矢久保美緒 blog 200427-1.jpg', as_attachment=True,
+                     download_name='矢久保美緒 blog 200427-1.jpg')
+
+
+@param.route('/img3', methods=['GET'])
+def down_img3():
+    url = 'https://www.nogizaka46.com/files/46/diary/n46/MEMBER/moblog/202301/mobxi6DOY.jpg'
+    response = requests.get(url=url)
+    img_data: bytes = response.content
+    resp = Response(img_data, mimetype="image/jpeg", direct_passthrough=True)
+    return resp
+
+
+@param.route('/img4', methods=['GET'])
+def down_img4():
+    url = 'https://www.nogizaka46.com/files/46/diary/n46/MEMBER/moblog/202301/mobxi6DOY.jpg'
+    response = requests.get(url=url)
+    img_data: bytes = response.content
+    byio = BytesIO(img_data)
+
+    return send_file(path_or_file=byio, as_attachment=True,
+                     download_name='佐藤璃果 blog 230125-1.jpg')
+
+
+@param.route('/zip', methods=['GET'])
+def down_zip():
+    memory_file = BytesIO()
+    with zipfile.ZipFile(memory_file, 'w') as zf:
+        files = [f for f in os.listdir('./') if re.match('test.jpg', f)]
+        for individualFile in files:
+            zf.write(individualFile, compress_type=zipfile.ZIP_DEFLATED)  # 壓縮種類
+        zf.close()
+    memory_file.seek(0)  # 改變當前讀寫位置。如果將參數設為 0，則會將讀寫位置移動到流的開頭
+    return send_file(memory_file, download_name='test.zip', as_attachment=True)
+
+
+@param.route('/blog_single')
+def blog_single_route():
+    req: dict = request.json
+    url = req.get('url')
+    print(url)
+    blog_single.download(url)
+    return None
+
+
+@param.route('/blog_single/photo', methods=['GET'])
+def blog_single_photo_route():
+    req: dict = request.json
+    url = req.get('url')
+    print(url)
+    zip_rs = blog_single_photo.download(url)
+    return send_file(zip_rs.zip_file, download_name=zip_rs.file_name, as_attachment=True)
